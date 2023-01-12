46c34888293f958f30f0a2c13861ae1aaabf94f6
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-11 22:04:51 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-/// A derived key represented by a fixed array of bytes.
-pub type Key<const SIZE: usize> = [u8; SIZE];
-
 /// A Key Derivation Function that procudes a `Key` of KEY_SIZE bytes from the given key material, salt, and info.
 pub trait Kdf {
     /// Derive a key from the given key material, salt, and info.
