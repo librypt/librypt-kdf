aba2dd858dbfb9e6da368ee0c7f04ab02eb59106
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-11 20:19:17 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,6 @@ pub type Key<const SIZE: usize> = [u8; SIZE];
 
 /// A Key Derivation Function that procudes a `Key` of KEY_SIZE bytes from the given key material, salt, and info.
 pub trait Kdf {
-    /// Derive a key of KEY_SIZE bytes from the given key material, salt, and info.
-    fn kdf<const KEY_SIZE: usize>(km: &[u8], salt: &[u8], info: &[u8]) -> Key<KEY_SIZE>;
+    /// Derive a key from the given key material, salt, and info.
+    fn kdf(ikm: &[u8], salt: &[u8], info: &[u8], okm: &mut [u8]);
 }
